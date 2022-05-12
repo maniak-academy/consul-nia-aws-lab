07df52aedf88be966bb6611ff58049e418d5521b
Sebbycorp <sebastian@maniak.io>
2022-05-12 12:22:16 +0000

    updates

--- f5-bigip.tf
+++ f5-bigip.tf
@@ -19,13 +19,11 @@ data "template_file" "f5_init" {
 module "bigip" {
   count                  = 1
   source                 = "F5Networks/bigip-module/aws"
-#  prefix                 = "${var.prefix}-f5-bigip"
-  prefix                 = "bigip-aws-1nic"
+  prefix                 = "${var.prefix}-f5-bigip"
   ec2_instance_type      = "m5.large"
   ec2_key_name           = aws_key_pair.demo.key_name
   f5_ami_search_name     = var.f5_ami_search_name
   f5_username            = var.f5_username
-  f5_password            = random_string.password.result
   mgmt_subnet_ids        = [{ "subnet_id" = module.vpc.public_subnets[0], "public_ip" = true, "private_ip_primary" = "10.0.0.200" }]
   mgmt_securitygroup_ids = [aws_security_group.f5.id]
   custom_user_data       = data.template_file.f5_init.rendered
